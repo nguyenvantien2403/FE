bd3b6f5ce106312108bc29a53bfa6ddc842cb5f6
TechNguyen <ndt13102003@gmail.com>
2024-06-18 10:50:47 +0700

    1

--- src/components/CardItem/CardItem.js
+++ src/components/CardItem/CardItem.js
@@ -4,58 +4,76 @@ import { addProductToCart } from "../../services/redux/cartSlice/productSlice";
 import { v4 as uuidv4 } from "uuid";
 import { Link } from "react-router-dom";
 const CardItem = ({ id, imgSrc, name, description, price }) => {
-  const dispatch = useDispatch();
+    const dispatch = useDispatch();
 
-  function formatCurrencyVND(amount) {
-    return new Intl.NumberFormat('vi-VN', { style: 'currency', currency: 'VND' }).format(amount);
-  } 
+    function formatCurrencyVND(amount) {
+        return new Intl.NumberFormat("vi-VN", {
+            style: "currency",
+            currency: "VND",
+        }).format(amount);
+    }
 
-  const handleAddToCart = () => {
-    dispatch(
-      addProductToCart({
-        id,
-        imgSrc,
-        name,
-        description,
-        price,
-        count: 1,
-      })
-    );
-  };
-  return (
-    <div className="col-md-6 col-lg-6 col-xl-3" >
-      <Link to={`/product/${id}`}>
-      <div className="rounded position-relative frute-item">
-        <div className="fruite-img">
-          <img style={{borderTop:"1px solid #FFC75A",borderLeft:"1px solid #FFC75A",borderRight:"1px solid #FFC75A", width:"300px", height:"209.8px", objectFit:"contain"}} src={imgSrc} className="img-fluid w-100 rounded-top" alt="" />
-        </div>
-        {/* <div
+    const handleAddToCart = () => {
+        dispatch(
+            addProductToCart({
+                id,
+                imgSrc,
+                name,
+                description,
+                price,
+                count: 1,
+            }),
+        );
+    };
+    return (
+        <div className="col-md-6 col-lg-6 col-xl-3">
+            <Link to={`/product/${id}`}>
+                <div className="rounded position-relative frute-item">
+                    <div className="fruite-img">
+                        <img
+                            style={{
+                                borderTop: "1px solid #FFC75A",
+                                borderLeft: "1px solid #FFC75A",
+                                borderRight: "1px solid #FFC75A",
+                                width: "300px",
+                                height: "209.8px",
+                                objectFit: "contain",
+                            }}
+                            src={imgSrc}
+                            className="img-fluid w-100 rounded-top"
+                            alt=""
+                        />
+                    </div>
+                    {/* <div
           className="text-white bg-secondary px-3 py-1 rounded position-absolute"
           style={{ top: "10px", left: "10px" }}
         >
           Fruits
         </div> */}
-        <div className="p-4 border border-secondary border-top-0 rounded-bottom">
-          {/* <h4 style={{lineHeight:"48px"}}>{name}</h4> */}
-          <h4 style={{ height:"72px", color:"black"}}>{name}</h4>
+                    <div className="p-4 border border-secondary border-top-0 rounded-bottom">
+                        {/* <h4 style={{lineHeight:"48px"}}>{name}</h4> */}
+                        <h4 style={{ height: "72px", color: "black" }}>
+                            {name}
+                        </h4>
 
-          {/* <p>{description}</p> */}
-          <div className="d-flex justify-content-between flex-lg-wrap">
-            <p className="text-dark fs-5 fw-bold mb-0">{formatCurrencyVND(price)}</p>
-            <div
-              className="btn border border-secondary rounded-pill px-3 text-primary"
-              onClick={handleAddToCart}
-            >         
-              <i className="fa fa-shopping-bag me-2 text-primary"></i> Add to
-              cart
-            </div>
-          </div>
+                        {/* <p>{description}</p> */}
+                        <div className="d-flex justify-content-between flex-lg-wrap">
+                            <p className="text-dark fs-5 fw-bold mb-0">
+                                {formatCurrencyVND(price)}
+                            </p>
+                            <div
+                                className="btn border border-secondary rounded-pill px-3 text-primary"
+                                onClick={handleAddToCart}
+                            >
+                                <i className="fa fa-shopping-bag me-2 text-primary"></i>{" "}
+                                Add to cart
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </Link>
         </div>
-      </div>
-      </Link>
-     
-    </div>
-  );
+    );
 };
 
 export default CardItem;
--- src/views/components/Dashboard/OrderManager/DetailOrder.js
+++ src/views/components/Dashboard/OrderManager/DetailOrder.js
@@ -1,160 +1,185 @@
-import useOrder from '@api/useOrder';
-import { render } from '@testing-library/react';
-import { Button,Modal,Form,Row, Col,Input, Select, Space, Table, Tag, Dropdown  } from 'antd';
-import { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
-import { toast } from 'react-toastify';
+import useOrder from "@api/useOrder";
+import { render } from "@testing-library/react";
+import {
+    Button,
+    Modal,
+    Form,
+    Row,
+    Col,
+    Input,
+    Select,
+    Space,
+    Table,
+    Tag,
+    Dropdown,
+} from "antd";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { toast } from "react-toastify";
 
 function formatCurrencyVND(amount) {
-  return new Intl.NumberFormat('vi-VN', { style: 'currency', currency: 'VND' }).format(amount);
-} 
+    return new Intl.NumberFormat("vi-VN", {
+        style: "currency",
+        currency: "VND",
+    }).format(amount);
+}
 function DetailOrder() {
     const params = useParams();
-    const {getDetail,edit} = useOrder();
-    const [order,setOrders] = useState({})
-    const [loading,setLoading] = useState(true)
+    const { getDetail, edit } = useOrder();
+    const [order, setOrders] = useState({});
+    const [loading, setLoading] = useState(true);
     const fetchData = async () => {
-      const {success,data} = await getDetail(params)
-      if (success && data.status != 'Error') {
-        toast.success(data.message)
-        setOrders(data.data)
-        setLoading(false)
-      } else {
-        toast.error(data.message)
-      }
-    }
+        const { success, data } = await getDetail(params);
+        if (success && data.status != "Error") {
+            if (loading) {
+                setLoading(false);
+                toast.success(data.message);
+            }
+            setOrders(data.data);
+        } else {
+            toast.error(data.message);
+        }
+    };
 
     const [modal2Open, setModal2Open] = useState(false);
 
-
-    const [status, setStatus] = useState('');
+    const [status, setStatus] = useState("");
     const handleSetStatus = async () => {
-      const {success,data} = await edit({id: order.id},status)
-      if(success && data.status != 'Error') {
-        // toast.success(data.message)
-        toast.success('update order success')
+        const { success, data } = await edit({ id: order.id }, status);
+        if (success && data.status != "Error") {
+            // toast.success(data.message)
+            toast.success("update order success");
 
-        setTimeout(() => {
-          setModal2Open(false)
-          setLoading(false)
-        }, 2500 );
-      } else {
-        toast.error(data.message)
-      }
-    }
-    const handChangeValue  = (value) => {
-      setStatus(value)
-    }
-    useEffect(()=> {
-      fetchData()
-    }, [loading])
+            setTimeout(() => {
+                setModal2Open(false);
+                setLoading(false);
+            }, 2500);
+        } else {
+            toast.error(data.message);
+        }
+    };
+    const handChangeValue = (value) => {
+        setStatus(value);
+    };
+    useEffect(() => {
+        fetchData();
+    }, [loading]);
     const columns = [
-      {
-        title: 'Product',
-        dataIndex: 'productName',
-        key: 'name',
-      },
-      {
-        title: 'Count',
-        dataIndex: 'count',
-        key: 'age',
-      },
-      {
-        title: 'Total',
-        dataIndex: 'total',
-        key: 'age',
-        render: (_, record) => (
-          (<p>{formatCurrencyVND(record.count * record.price)}</p>)
-        )
-      },
+        {
+            title: "Product",
+            dataIndex: "productName",
+            key: "name",
+        },
+        {
+            title: "Count",
+            dataIndex: "count",
+            key: "age",
+        },
+        {
+            title: "Total",
+            dataIndex: "total",
+            key: "age",
+            render: (_, record) => (
+                <p>{formatCurrencyVND(record.count * record.price)}</p>
+            ),
+        },
     ];
-    return ( 
-      <div> 
-        <fieldset>
-          <legend>
-            <p>Order detail Information</p>
-          </legend>
 
-          <Row>
-            <Col span={16}>
-            <span>
-              <p>User: {order.firstName} {order.lastName}</p>
-              <p>Phone: {order.mobile} </p>
-              <p>Note : {order.orderNotes} </p>
-              <p>Create Date : {order.createat} </p>
-              <p>Email : {order.email} </p>
-              <p>Address : {order.address} </p>
-              <p>Total : {formatCurrencyVND(order.totalPrice)} </p>
-              <p>Status :     <Tag color="volcano">{order.status} </Tag> </p>
-            </span>
-            </Col>
+    console.log(order);
+    return (
+        <div>
+            <fieldset>
+                <legend>
+                    <p>Order detail Information</p>
+                </legend>
 
-            <Col span={3}>
-              <Button  type="primary" value="large" style={{background: 'green'}}  onClick={() => setModal2Open(true)}>
-                  Update status
-              </Button>
-              <Modal
-                 width={'50%'}
-                 centered
-                 visible={modal2Open}
-                 onCancel={() => setModal2Open(false)}
-                 onOk={() => handleSetStatus()}
-              >
-                <Select
-                    placeholder="Chang status"
-                    defaultValue={order.status}
-                    onChange={handChangeValue}
-                    options={[
-                      // {
-                      //   value: 'DANGCHUANBI',
-                      //   label: 'Đang chuẩn bị',
-                      // },
-                      {
-                        value: 'CHOXACNHAN',
-                        label: 'Chờ xác nhận',
-                      },
-                      // {
-                      //   value: 'DANGIAO',
-                      //   label: 'Đang giao',
-                      // },
-                      {
-                        value: 'DANHAN',
-                        label: 'Đã xác nhận',
-                      },
-                      // {
-                      //   value: 'DAGUI',
-                      //   label: 'Đã gửi',
-                      // },
-                      // {
-                      //   value: 'DATHANHTOAN',
-                      //   label: 'Đã thanh toán',
-                      // },
-                      {
-                        value: 'THANHCONG',
-                        label: 'Thành công',
-                      },
-                      // {
-                      //   value: 'DAHUY',
-                      //   label: 'Đã hủy',
-                      // },
-                      {
-                        value: 'THATBAI',
-                        label: 'Thất bại',
-                      },
-                    ]}
-                  />
-              </Modal>
-            </Col>
-          </Row>
+                <Row>
+                    <Col span={16}>
+                        <span>
+                            <p>User: {order.username}</p>
+                            <p>Full Name: {order.firstName}</p>
+                            <p>Phone: {order.mobile} </p>
+                            <p>Create Date : {order.createat} </p>
+                            <p>Address : {order.address} </p>
+                            <p>
+                                Total : {formatCurrencyVND(order.totalPrice)}{" "}
+                            </p>
+                            <p>
+                                Status :{" "}
+                                <Tag color="volcano">{order.status} </Tag>{" "}
+                            </p>
+                        </span>
+                    </Col>
 
-          <span>
-          <Table dataSource={order.cartDtos} columns={columns} />
-          </span>
-        </fieldset>
+                    <Col span={3}>
+                        <Button
+                            type="primary"
+                            value="large"
+                            style={{ background: "green" }}
+                            onClick={() => setModal2Open(true)}
+                        >
+                            Update status
+                        </Button>
+                        <Modal
+                            width={"50%"}
+                            centered
+                            visible={modal2Open}
+                            onCancel={() => setModal2Open(false)}
+                            onOk={() => handleSetStatus()}
+                        >
+                            <Select
+                                placeholder="Chang status"
+                                defaultValue={order.status}
+                                onChange={handChangeValue}
+                                options={[
+                                    // {
+                                    //   value: 'DANGCHUANBI',
+                                    //   label: 'Đang chuẩn bị',
+                                    // },
+                                    {
+                                        value: "CHOXACNHAN",
+                                        label: "Chờ xác nhận",
+                                    },
+                                    // {
+                                    //   value: 'DANGIAO',
+                                    //   label: 'Đang giao',
+                                    // },
+                                    {
+                                        value: "DANHAN",
+                                        label: "Đã xác nhận",
+                                    },
+                                    // {
+                                    //   value: 'DAGUI',
+                                    //   label: 'Đã gửi',
+                                    // },
+                                    // {
+                                    //   value: 'DATHANHTOAN',
+                                    //   label: 'Đã thanh toán',
+                                    // },
+                                    {
+                                        value: "THANHCONG",
+                                        label: "Thành công",
+                                    },
+                                    // {
+                                    //   value: 'DAHUY',
+                                    //   label: 'Đã hủy',
+                                    // },
+                                    {
+                                        value: "THATBAI",
+                                        label: "Thất bại",
+                                    },
+                                ]}
+                            />
+                        </Modal>
+                    </Col>
+                </Row>
 
-
-     
-     </div> );
+                <span>
+                    <Table dataSource={order.cartDtos} columns={columns} />
+                </span>
+            </fieldset>
+        </div>
+    );
 }
 
 export default DetailOrder;
